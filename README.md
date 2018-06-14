6b345088e231aa00980a6b5841ebb7b4e7a8e5d2
David Witter <david.witter@wisc.edu>
2018-06-14 14:26:48 -0500

    switch to js-only

---
+++ README.md
@@ -0,0 +1,11 @@
+# MyUW App Styles
+
+This HTML import contains custom CSS properties that are consumed by other components in the myuw-web-components space. 
+
+## How to use
+
+Add the app styles script to your document:
+
+```html
+<script rel="import" href="https://unpkg.com/@myuw-web-components/myuw-app-styles@1.0.0/myuw-app-styles.js"><script>
+```
---
+++ myuw-app-styles.js
@@ -0,0 +1,27 @@
+// Add MyUW brand fonts to the document
+const MyuwfontsContainer = document.createElement('template');
+MyuwfontsContainer.setAttribute('style', 'display: none;');
+MyuwfontsContainer.innerHTML = `
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+`
+    
+
+const MyuwDefaultAppTheme = document.createElement('template');
+MyuwDefaultAppTheme.innerHTML = `
+    <style>
+        myuw-app-bar {
+            --myuw-app-bar-bg: #c5050c;
+            --myuw-app-bar-color: #fff;
+            --myuw-app-bar-font: 'Montserrat', Arial, sans-serif;
+        }
+        myuw-profile {
+            --myuw-profile-font: 'Montserrat', Arial, sans-serif;
+            --myuw-profile-color: #fff;
+        }
+    </style>
+`;
+    
+// Add templates to the document 
+document.head.appendChild(MyuwfontsContainer.content);
+document.head.appendChild(MyuwDefaultAppTheme.content);
